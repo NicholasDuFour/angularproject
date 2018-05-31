778ae895bf52795c61193c754688683ecac40e8e
Ashley <ashleyvport@gmail.com>
2018-05-31 13:54:20 -0400

    can click details

--- service.js
+++ service.js
@@ -7,7 +7,7 @@ function MovieService($http) {
     // console.log(title);
     let movieTitle = title;
     return $http ({
-      url: "https://api.themoviedb.org/3/search/movie?api_key=&query=" + title,
+      url: "https://api.themoviedb.org/3/search/movie?api_key=32535e85345be91dd928261c4d1d2a0e&query=" + title,
       method: "GET"
     }).then((response) =>{
       movieData = response;
@@ -20,7 +20,7 @@ function MovieService($http) {
     // console.log(movieid);
 
     return $http ({
-      url: "https://api.themoviedb.org/3/movie/"+movieid+"?api_key=&append_to_response=releases",
+      url: "https://api.themoviedb.org/3/movie/"+movieid+"?api_key=32535e85345be91dd928261c4d1d2a0e&append_to_response=releases",
       method: "GET"
     }).then((response) =>{
       movieDetailData = response;
